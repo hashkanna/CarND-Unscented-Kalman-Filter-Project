af7160fe9f502b48e7457981977b46722c49b0ea
Kannappan Sirchabesan <hashkanna@gmail.com>
2017-06-27 07:51:06 +0100

    initialise variables

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -24,10 +24,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 0.2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.2;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -51,6 +51,24 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  is_initialized_ = false;
+
+  //set state dimension
+  int n_x_ = 5;
+
+  //set augmented dimension
+  int n_aug_ = 7;
+
+  //define spreading parameter
+  double lambda_ = 3 - n_aug_;
+
+  //create vector for weights
+  weights_ = VectorXd(2*n_aug_+1);
+
+  // predicted sigma points matrix
+  MatrixXd Xsig_pred_;
+
 }
 
 UKF::~UKF() {}
